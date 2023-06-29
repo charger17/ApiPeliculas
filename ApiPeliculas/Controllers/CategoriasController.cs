e38cbda7d8e3a29f34293a8ac078da56d563c493
Carlos Perez Salazar <cps172779@gmail.com>
2023-06-29 13:53:10 -0600

    get categoria by id

--- ApiPeliculas/Controllers/CategoriasController.cs
+++ ApiPeliculas/Controllers/CategoriasController.cs
@@ -1,4 +1,5 @@
-﻿using ApiPeliculas.Models.Dtos;
+﻿using ApiPeliculas.Models;
+using ApiPeliculas.Models.Dtos;
 using ApiPeliculas.Repository.IRepository;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
@@ -29,6 +30,24 @@ namespace ApiPeliculas.Controllers
             listaCategoriasDto = _mapper.Map<List<CategoriaDto>>(listaCategorias);
 
             return Ok(listaCategoriasDto);
+        }
+
+        [HttpGet("{categoriaId:int}", Name = "GetCategoria")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult GetCategoria(int categoriaId)
+        {
+            var itemCategoria = _ctRepo.GetCategoria(categoriaId);
+
+            if (itemCategoria == null)
+            {
+                return NotFound();
+            }
+
+            var itemCategoriaDto = _mapper.Map<CategoriaDto>(itemCategoria);
+
+            return Ok(itemCategoriaDto);
         }
     }
 }
