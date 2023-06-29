a94628096082f6098fc0b2151225cd1ddb31c3ed
Carlos Perez Salazar <cps172779@gmail.com>
2023-06-29 17:13:30 -0600

    Add pelicula repository

---
+++ ApiPeliculas/Repository/IRepository/IPeliculaRepository.cs
@@ -0,0 +1,28 @@
+﻿using ApiPeliculas.Models;
+
+namespace ApiPeliculas.Repository.IRepository
+{
+    public interface IPeliculaRepository
+    {
+        ICollection<Pelicula> GetPeliculas();
+
+        Pelicula GetPelicula(int peliculaId);
+
+        bool ExistePelicula(string nombre);
+
+        bool ExistePelicula(int id);
+
+        bool CrearPelicula(Pelicula pelicula);
+        
+        bool ActualizarPelicula(Pelicula pelicula);
+
+        bool BorrarPelicula(Pelicula pelicula);
+
+        //Métodos para buscar peliculas en categorias y buscar pelicula por nombre
+        ICollection<Pelicula> GetPeliculasEnCategoria(int categoriaId);
+        ICollection<Pelicula> BuscarPelicula(string nombre);
+
+
+        bool Guardar();
+    }
+}
