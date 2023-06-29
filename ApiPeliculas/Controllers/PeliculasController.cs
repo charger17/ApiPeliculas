73227bf6f06f657e36a4dba499d4815a921c28e5
Carlos Perez Salazar <cps172779@gmail.com>
2023-06-29 17:16:56 -0600

    Add pelicula controller

---
+++ ApiPeliculas/Controllers/PeliculasController.cs
@@ -0,0 +1,23 @@
+﻿using ApiPeliculas.Repository.IRepository;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiPeliculas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeliculasController : ControllerBase
+    {
+        private readonly IPeliculaRepository _pelRepo;
+        private readonly IMapper _mapper;
+
+        public PeliculasController(IPeliculaRepository pelRepo, IMapper mapper)
+        {
+            _pelRepo = pelRepo;
+            _mapper = mapper;
+        }
+
+
+    }
+}
