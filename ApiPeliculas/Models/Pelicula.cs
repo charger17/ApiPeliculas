66add8fb4e4633b144b883ee4679a18ead5dad3d
Carlos Perez Salazar <cps172779@gmail.com>
2023-06-29 16:27:43 -0600

    Add pelicula model

---
+++ ApiPeliculas/Models/Pelicula.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiPeliculas.Models
+{
+    public class Pelicula
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string RutaImagen { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public int Duracion { get; set; }
+
+        public enum tipoClasificacion
+        {
+            Siete,
+            Trece,
+            Dieciseis,
+            Dieciocho
+        } 
+
+        public tipoClasificacion Clasificacion { get; set; }
+
+        public DateTime FechaCreacion { get; set; }
+
+        [ForeignKey("categoriaId")]
+        public int CategoriaId { get; set; }
+
+        public Categoria Categoria { get; set; }
+
+
+    }
+}
