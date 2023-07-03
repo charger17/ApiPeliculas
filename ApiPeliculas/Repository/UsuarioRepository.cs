95d291476ee7aa35fdb580f68107cd4237c03893
Carlos Perez Salazar <cps172779@gmail.com>
2023-07-03 17:05:59 -0600

    add login method

--- ApiPeliculas/Repository/UsuarioRepository.cs
+++ ApiPeliculas/Repository/UsuarioRepository.cs
@@ -4,6 +4,10 @@ using ApiPeliculas.Models.Dtos;
 using ApiPeliculas.Repository.IRepository;
 using AutoMapper;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using XSystem.Security.Cryptography;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
@@ -13,11 +17,13 @@ namespace ApiPeliculas.Repository
     {
         private readonly ApplicationDbContext _bd;
         private readonly IMapper _mapper;
+        private string claveSecreta;
 
-        public UsuarioRepository(ApplicationDbContext bd, IMapper mapper)
+        public UsuarioRepository(ApplicationDbContext bd, IMapper mapper, IConfiguration configuration)
         {
             _bd = bd;
             _mapper = mapper;
+            claveSecreta = configuration.GetValue<string>("ApiSettings:Secreta");
         }
 
         public Usuario GetUsuario(int usuarioId)
@@ -42,7 +48,7 @@ namespace ApiPeliculas.Repository
             var passwordEncriptado = obtenerMD5(usuarioRegistroDto.Password);
 
             Usuario usuario = new Usuario();
-            
+
             usuario = _mapper.Map<Usuario>(usuarioRegistroDto);
             usuario.Password = passwordEncriptado;
 
@@ -53,11 +59,48 @@ namespace ApiPeliculas.Repository
             return usuario;
         }
 
-        public Task<UsuarioLoginRespuestaDto> Login(UsuarioLoginDto usuarioLoginDto)
+        public async Task<UsuarioLoginRespuestaDto> Login(UsuarioLoginDto usuarioLoginDto)
         {
-            throw new NotImplementedException();
-        } 
-        
+            var passwordEncriptado = obtenerMD5(usuarioLoginDto.Password);
+
+            var usuario = await _bd.Usuarios.FirstOrDefaultAsync(u => u.NombreUsuario.ToLower().Equals(usuarioLoginDto.NombreUsuario.ToLower()) && u.Password.Equals(passwordEncriptado));
+
+            //validamos si el usuario no existe con la combianción correcta
+            if (usuario == null)
+            {
+                return new UsuarioLoginRespuestaDto()
+                {
+                    Token = "",
+                    Usuario = null
+                };
+            }
+
+            //aqui si existe el usuario entocnes podemos procesar el login
+            var manejadorToken = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(claveSecreta);
+
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, usuario.NombreUsuario.ToString()),
+                    new Claim(ClaimTypes.Role, usuario.Role)
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new (new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = manejadorToken.CreateToken(tokenDescriptor);
+
+            UsuarioLoginRespuestaDto usuarioLoginRespuestaDto = new UsuarioLoginRespuestaDto() 
+            {
+                Token = manejadorToken.WriteToken(token),
+                Usuario = usuario
+            };
+
+            return usuarioLoginRespuestaDto;
+        }
+
 
         /// <summary>
         /// Metodo para encriptar contraseña
