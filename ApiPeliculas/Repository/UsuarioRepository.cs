5cb43c4d739141f3ae033a4370334366f398a8b5
Carlos Perez Salazar <cps172779@gmail.com>
2023-07-03 16:00:19 -0600

    Add user interface

---
+++ ApiPeliculas/Repository/UsuarioRepository.cs
@@ -0,0 +1,46 @@
+﻿using ApiPeliculas.Data;
+using ApiPeliculas.Models;
+using ApiPeliculas.Models.Dtos;
+using ApiPeliculas.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ApiPeliculas.Repository
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private readonly ApplicationDbContext _bd;
+
+        public UsuarioRepository(ApplicationDbContext bd)
+        {
+            _bd = bd;
+        }
+
+        public Usuario GetUsuario(int usuarioId)
+        {
+            return _bd.Usuarios.FirstOrDefault(x => x.Id.Equals(usuarioId));
+        }
+
+        public ICollection<Usuario> GetUsuarios()
+        {
+            return _bd.Usuarios.OrderBy(u => u.NombreUsuario).ToList();
+        }
+
+        public bool IsUniqueUser(string nombre)
+        {
+            var usuarioBd = _bd.Usuarios.FirstOrDefault(x => x.NombreUsuario.Equals(nombre));
+
+            return usuarioBd is null ? true : false;
+        }
+
+        public Task<UsuarioLoginRespuestaDto> Login(UsuarioLoginDto usuarioLoginDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Usuario> Registro(UsuarioRegistroDto usuarioRegistroDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
