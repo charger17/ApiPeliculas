02d3f66bbf37395c5a454ffa9b31c007cb207b60
Carlos Perez Salazar <cps172779@gmail.com>
2023-07-03 18:58:38 -0600

    Add documentation for Bearer in SwaggerGen

--- ApiPeliculas/Program.cs
+++ ApiPeliculas/Program.cs
@@ -5,6 +5,7 @@ using ApiPeliculas.Repository.IRepository;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -49,7 +50,37 @@ builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+//add documentation for JWT
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Description= "Autenticación JWT usando el esquema Bearer. \r\n\r" + 
+        "Ingresa la palabra Beare seguida de un espacio y después su token en el campo de abajo \r\n\r" +
+        "Ejemplo: \"Bearer dafasdfasdfas\"" ,
+        Name="Authorization",
+        In = ParameterLocation.Header,
+        Scheme = "Bearer"
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme()
+            {
+                Reference = new OpenApiReference()
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id ="Bearer"
+                },
+                Scheme ="oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
 
 //SOPORTE PARA CORS
 /*
